bc027a806ffdb72a65ea75b832c8f16c365e92c7
mahaveer3 <mahaveerkankaria6@gmail.com>
2025-02-04 08:41:25 +0530

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NODE_ENV === 'production' 
+const API_URL = window.location.hostname === 'rootaccess.vercel.app'
   ? 'https://rootaccess.vercel.app'
   : 'http://127.0.0.1:5000';
 
